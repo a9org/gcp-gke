917a83d683e1f3f87d1370973e6871083ad34505
Leonardo Oliveira <leonardo@a9tech.com.br>
2024-11-08 11:45:53 -0300

    fix: ajuste na identacao

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
-    common_tags = {
-        owner = var.owner
-        project = var.project
-        environment = var.environment
-    }
+  common_tags = {
+    owner       = var.owner
+    project     = var.project
+    environment = var.environment
+  }
 
-    prefix_name = "${var.project}-${var.environment}"
+  prefix_name = "${var.project}-${var.environment}"
 }
